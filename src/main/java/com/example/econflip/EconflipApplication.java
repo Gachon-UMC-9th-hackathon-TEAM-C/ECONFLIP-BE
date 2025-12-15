08d560870953d1e8e83f15238673c22be670bad0
kangyeson <yesonkang@gmail.com>
2025-12-15 21:48:33 +0900

    Initial Settings

---
+++ src/main/java/com/example/econflip/EconflipApplication.java
@@ -0,0 +1,13 @@
+package com.example.econflip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconflipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconflipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/econflip/EconflipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.econflip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconflipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
