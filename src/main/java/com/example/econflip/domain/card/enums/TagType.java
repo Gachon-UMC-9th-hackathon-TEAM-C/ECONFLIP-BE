76ce851d920e7c86e6be86cab6c286722c996711
kangyeson <yesonkang@gmail.com>
2025-12-16 21:39:04 +0900

    [Add] #18 Card, Tag Entity, TagType Enum 추가

---
+++ src/main/java/com/example/econflip/domain/card/enums/TagType.java
@@ -0,0 +1,8 @@
+package com.example.econflip.domain.card.enums;
+
+public enum TagType {
+    INTEREST_RATE,   // 금리
+    INFLATION,       // 물가
+    INVESTMENT,      // 투자
+    FISCAL           // 재정
+}
