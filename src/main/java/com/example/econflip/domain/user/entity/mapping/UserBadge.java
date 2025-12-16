4989ce2f54a4b5a49903d1a8ebfa9b63e1032593
kangyeson <yesonkang@gmail.com>
2025-12-16 22:06:44 +0900

    [Add] #18 Badge, Title, UserBadge, UserTitle Entity 추가

---
+++ src/main/java/com/example/econflip/domain/user/entity/mapping/UserBadge.java
@@ -0,0 +1,26 @@
+package com.example.econflip.domain.user.entity.mapping;
+
+import com.example.econflip.domain.user.entity.Badge;
+import com.example.econflip.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "user_badge")
+public class UserBadge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "badge_id")
+    private Badge badge;
+}
---
+++ src/main/java/com/example/econflip/domain/user/entity/mapping/UserTitle.java
@@ -0,0 +1,26 @@
+package com.example.econflip.domain.user.entity.mapping;
+
+import com.example.econflip.domain.user.entity.Title;
+import com.example.econflip.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "user_title")
+public class UserTitle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "title_id")
+    private Title title;
+}
