33644d6bccc6c796f381e477bd69bc5f9d8784fb
kangyeson <yesonkang@gmail.com>
2025-12-16 22:00:29 +0900

    [Add] #18 Quiz Entity, QuizType Enum 추가

---
+++ src/main/java/com/example/econflip/domain/card/entity/Quiz.java
@@ -0,0 +1,33 @@
+package com.example.econflip.domain.card.entity;
+
+import com.example.econflip.domain.card.enums.QuizType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "quiz")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "card_id", nullable = false)
+    private Long cardId;
+
+    @Column(name = "answer", length = 255, nullable = false)
+    private String answer;
+
+    @Column(name = "is_answer", nullable = false)
+    private Boolean is_answer;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private QuizType quizType;
+
+    @Column(name = "commentary", length = 255, nullable = false)
+    private String commentary;
+}
