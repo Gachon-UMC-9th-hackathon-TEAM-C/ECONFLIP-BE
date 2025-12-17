52d446dce232c5d81405c72094fde05251b01aec
Hanharam <hanharamm@naver.com>
2025-12-17 23:01:17 +0900

    [Delete] #27 불필요한 주석 삭제

--- src/main/java/com/example/econflip/domain/user/dto/UserResDTO.java
+++ src/main/java/com/example/econflip/domain/user/dto/UserResDTO.java
@@ -22,11 +22,3 @@ public class UserResDTO {
             Integer dailyStudy
     ){}
 }
-// 마이페이지: 유저 칭호 + xp + 레벨 + 다음 레벨까지 남은 xp + 레벨별 진행률 + 연속학습 + 학습한 용어 개수 + 획득한 배지() + 북마크한 용어
-//return ReviewResDTO.MyReviewDTO.builder()
-//                .userName(review.getMember().getName())
-//                .storeName(review.getStore().getName())
-//                .star(review.getStar())
-//                .body(review.getContent())
-//                .createdAt(LocalDate.from(review.getCreatedAt()))
-//                .build();
