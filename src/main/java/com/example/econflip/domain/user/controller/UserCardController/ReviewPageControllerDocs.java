5bc3d47268f671129f01a6c51b8c9ddc282dd37a
gyeonseo <phsphg77@naver.com>
2025-12-17 21:35:45 +0900

    docs - swagger 설명 추가

--- src/main/java/com/example/econflip/domain/user/controller/UserCardController/ReviewPageControllerDocs.java
+++ src/main/java/com/example/econflip/domain/user/controller/UserCardController/ReviewPageControllerDocs.java
@@ -1,6 +1,21 @@
 package com.example.econflip.domain.user.controller.UserCardController;
 
+import com.example.econflip.domain.user.dto.UserCardResDTO;
+import com.example.econflip.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Review", description = "복습 페이지 API")
 public interface ReviewPageControllerDocs {
 
     // api/review
+    @Operation(
+            summary = "복습 페이지 조회"
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
+    })
+    ApiResponse<UserCardResDTO.reviewPage> reviewPage(Long userId);
 }
