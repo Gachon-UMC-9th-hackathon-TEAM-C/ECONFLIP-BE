9cb9af72c75f3fcb811e189ef6c2d64a368b5204
kangyeson <yesonkang@gmail.com>
2025-12-21 03:29:04 +0900

    [Delete] CardControllerDocs 불필요한 코드 삭제

--- src/main/java/com/example/econflip/domain/card/controller/CardControllerDocs.java
+++ src/main/java/com/example/econflip/domain/card/controller/CardControllerDocs.java
@@ -52,5 +52,5 @@ public interface CardControllerDocs {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
     })
-    ApiResponse<CardResDTO.StudyComplete> completeStudy(User user,Long studySetId);
+    ApiResponse<CardResDTO.StudyComplete> completeStudy(User user);
 }
