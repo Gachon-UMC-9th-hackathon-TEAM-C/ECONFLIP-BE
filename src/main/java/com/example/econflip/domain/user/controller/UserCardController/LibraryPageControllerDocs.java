da063fd98eef5a9e74300d42518c06b2bb717190
gyeonseo <phsphg77@naver.com>
2025-12-21 02:35:52 +0900

    swagger 설명추가

--- src/main/java/com/example/econflip/domain/user/controller/UserCardController/LibraryPageControllerDocs.java
+++ src/main/java/com/example/econflip/domain/user/controller/UserCardController/LibraryPageControllerDocs.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Library", description = "라이브러리 페이지 API")
@@ -26,5 +28,19 @@ public interface LibraryPageControllerDocs {
     public ApiResponse<UserCardResDTO.libraryPage> libraryPage(
             User user,
             @RequestParam(required = false) CategoryType category
+    );
+
+    @Operation(
+            summary = "북마크 업데이트",
+            description = "해당 카드 아이디의 북마크를 업데이트합니다."
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
+    })
+    @PatchMapping("/user-card/{cardId}/bookmark")
+    public ApiResponse<UserCardResDTO.bookmarkClick> updateBookmark(
+            User user,
+            @PathVariable Long cardId
     );
 }
