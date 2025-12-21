03ac9bf249a5d4ecf1bb63d6dba453d94013c153
Hanharam <hanharamm@naver.com>
2025-12-21 22:48:27 +0900

    [Mod] excpetion

--- src/main/java/com/example/econflip/domain/user/repository/TitleRepository.java
+++ src/main/java/com/example/econflip/domain/user/repository/TitleRepository.java
@@ -4,8 +4,8 @@ import com.example.econflip.domain.user.entity.Title;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface TitleRepository extends JpaRepository<Title, Long> {
-    Optional<Title> findById(Long id);}
+}
